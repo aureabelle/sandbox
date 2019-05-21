2dad70cceb0c63902c119a8f0240b1bab89fabf8
aureabelle <acruz@inboxlab.com>
2019-05-21 15:00:25 -0400

    snackers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Search from './Search';
+
+import 'antd/dist/antd.css';
 import './app.css';
 
 const App = () => {
--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,37 @@
 import React, { Component } from 'react';
+import { Table, Select, Alert, Icon } from 'antd';
+
+const Option = Select.Option;
+const columns = [
+  {
+    title: 'First Name',
+    dataIndex: 'first_name',
+  },
+  {
+    title: 'Last Name',
+    dataIndex: 'last_name',
+  },
+  {
+    title: 'Favorite Snack',
+    dataIndex: 'fave_snack',
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+  },
+  {
+    title: 'IP',
+    dataIndex: 'ip_address',
+  },
+  {
+    title: 'ID',
+    dataIndex: 'id',
+  },
+];
 
 class Search extends Component {
   constructor(props) {
@@ -7,7 +40,7 @@ class Search extends Component {
       snackers: [],
       products: [],
       selectedSnack: '',
-      isSearching: false
+      isSearching: false,
     };
     this.handleSelectChange = this.handleSelectChange.bind(this);
   }
@@ -46,9 +79,8 @@ class Search extends Component {
       });
   }
 
-  handleSelectChange(event) {
-    const { selectedSnack } = this.state;
-    const value = event.target.value;
+  handleSelectChange(value) {
+    const { selectedSnack, snackers } = this.state;
     this.setState({
       selectedSnack: value
     });
@@ -57,46 +89,42 @@ class Search extends Component {
   render() {
     const { snackers, products, selectedSnack } = this.state;
     const filteredSnackers = snackers.filter(snacker => snacker.fave_snack === selectedSnack);
+    const snackersList = filteredSnackers.length !== 0 ? filteredSnackers : snackers;
+    const existInInventory = snackers.some(snacker => snacker.fave_snack === selectedSnack);
 
     return (
       <div className="container">
         <h1>Snackers & Snacks</h1>
-        <select onChange={this.handleSelectChange} value={selectedSnack}>
-          <option value=''>Select Snack</option>
-          {products.map((product, index) => {
-            return (
-              <option value={product.title}>{product.title} - {product.variants[0].price}</option>
-            );
-          })}
-        </select>
 
-        {filteredSnackers.length !== 0 &&
-          <table className="snackers">
-            <thead>
-              <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Favorite Snack</th>
-                <th>Email</th>
-                <th>Gender</th>
-                <th>IP</th>
-              </tr>
-            </thead>
-
-            {filteredSnackers.map((item, index) => {
+        <div className="dropdown">
+          <Select
+            defaultValue={selectedSnack}
+            onChange={this.handleSelectChange}
+          >
+            <Option value=''>Select Snack</Option>
+            {products.map((product, index) => {
               return (
-                <tr className="item" key={`item-${index}`}>
-                  <td>{item.first_name}</td>
-                  <td>{item.last_name}</td>
-                  <td>{item.fave_snack}</td>
-                  <td>{item.email}</td>
-                  <td>{item.gender}</td>
-                  <td>{item.ip_address}</td>
-                </tr>
+                <Option value={product.title}>{product.title} - {product.variants[0].price}</Option>
               );
             })}
-          </table>
+          </Select>
+        </div>
+
+        {(!existInInventory && selectedSnack !== '')  &&
+          <div className="message">
+            <Alert
+              message={`Nobody likes ${selectedSnack}.`}
+              type="error"
+              showIcon
+              icon={<Icon type="frown" />}
+            />
+          </div>
         }
+
+        <Table
+          dataSource={snackersList}
+          columns={columns}
+        />
       </div>
     );
   }
